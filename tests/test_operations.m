50a64db0c952407b2f59280fcdecdf48e77d6602
Leonardo Robol <leo@robol.it>
2015-03-09 10:50:20 +0100

    Improved test output.

--- tests/test_operations.m
+++ tests/test_operations.m
@@ -1,4 +1,10 @@
+ps = 2;
+
+try; 
+
 addpath('..');
+
+fprintf (ps, 'Testing the main operations of H2Lib\n\n')
 
 n = 100; 
 a = randn(n,1); b = randn(n-1,1); c = randn(n-1,1);
@@ -11,21 +17,25 @@ H2 = HMatrix('tridiagonal', cc, cc, a2, b2, c2);
 H3 = H * H2;
 
 res = norm(full(H3) - full(H) * full(H2));
-fprintf ('Residue of full(H*H2) - full(H)*full(H2) = %e\n', res);
+fprintf (ps, ' > Residue of full(H*H2) - full(H)*full(H2) = %e\n', res);
 
 H3 = H + H2;
 res = norm(full(H3) - full(H) - full(H2));
-fprintf ('Residue of full(H + H2) - full(H) - full(H2) = %e\n', res);
+fprintf (ps, ' > Residue of full(H + H2) - full(H) - full(H2) = %e\n', res);
 
 H3 = H \ H2;
 res = norm(full(H3) - full(H) \ full(H2));
-fprintf ('Residue of full(H + H2) - full(H) \\ full(H2) = %e\n', res);
+fprintf (ps, ' > Residue of full(H + H2) - full(H) \\ full(H2) = %e\n', res);
 
 H3 = H / H2;
 res = norm(full(H3) - full(H) / full(H2));
-fprintf ('Residue of full(H + H2) - full(H) / full(H2) = %e\n', res);
+fprintf (ps, ' > Residue of full(H + H2) - full(H) / full(H2) = %e\n', res);
 
 H3 = inv(H);
 res = norm(full(H3) * full(H) - eye(n));
-fprintf ('Residue of full(H * inv(H)) - I = %e\n', res);
+fprintf (ps, ' > Residue of full(H * inv(H)) - I = %e\n', res);
 
+fprintf (ps, '\n');
+
+catch;
+end;

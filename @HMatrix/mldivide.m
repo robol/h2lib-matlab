89ba914314aaace13386125748e2e25d9d0a2887
Stefano Massei <stefano.massei@sns.it>
2015-03-14 17:57:37 +0100

    Added missing files.

--- @HMatrix/mldivide.m
+++ @HMatrix/mldivide.m
@@ -1,7 +1,10 @@
 function M = mldivide(H, A)
     if (isa(A, 'HMatrix'))
-        Hinv = HMatrix('pointer', hmatrix_inv (H));
-        M = Hinv * A;
+        l = hmatrix_mldivide(H,A);
+        M = [];
+        for i = 1 : size(A,2)
+          M = [ M, HMatrix('pointer', l(i)) ];
+        end
     else
         % TODO: Implement this as a linear system solver. 
         M = full(H) \ A;

d23e595a09e66387a33362aec3a776b5befbbf53
Stefano Massei <stefano.massei@sns.it>
2016-07-26 18:26:29 +0200

    modified check in mtimies.m

--- @HMatrix/mtimes.m
+++ @HMatrix/mtimes.m
@@ -1,5 +1,19 @@
 function c = mtimes(H, b)
-
+ if (isa(b, 'HMatrix'))
+    % TODO: We need to check that dimensions and clusters match. 
+    Cptr = hmatrix_prod (H, b);
+    c = HMatrix('pointer', Cptr, H.row_cluster, b.col_cluster);
+    return;
+end
+if (ismatrix(b) && ~isscalar(b))
+    sz = size(b,2);
+    c=zeros(size(b,1),sz);
+    % TODO: Handle more columns as well
+    for j=1:sz
+      c(:,j) = mvm_hmatrix_avector(H, b(:,j));
+    end
+return;
+end	
   if isfloat(H) && isscalar(H)
       c = HMatrix('pointer', hmatrix_scale (b, H), b.row_cluster, b.col_cluster);
       return;
@@ -10,17 +24,5 @@ function c = mtimes(H, b)
       return;
   end
 
-  if (isa(b, 'HMatrix'))
-    % TODO: We need to check that dimensions and clusters match. 
-    Cptr = hmatrix_prod (H, b);
-    c = HMatrix('pointer', Cptr, H.row_cluster, b.col_cluster);
-  else
     
-    sz = size(b,2);
-    c=zeros(size(b,1),sz);
-    % TODO: Handle more columns as well
-    for j=1:sz
-      c(:,j) = mvm_hmatrix_avector(H, b(:,j));
-    end
-  end	   
 end
